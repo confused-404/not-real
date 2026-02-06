7ead8120e919c39f90d57b7860224aa6b12fbfef
Yves Kloeckner <hosta.gruen@gmail.com>
2026-02-05 22:52:42 -0700

    fixed most broken ops

--- ops.c
+++ ops.c
@@ -27,7 +27,7 @@ void op_and(uint16_t reg[], uint16_t instr) {
         uint16_t imm5 = sign_extend(instr & 0x1F, 5);
         reg[dr] = reg[sr1] & imm5;
     } else {
-        reg[dr] = reg[sr1] & (instr & 0x7); 
+        reg[dr] = reg[sr1] & reg[(instr & 0x7)]; 
     }
 
     update_flags(reg, dr);
@@ -66,30 +66,64 @@ void op_jsr(uint16_t reg[], uint16_t instr) {
 }
 
 void op_ld(uint16_t reg[], uint16_t instr) {
-    uint16_t dr = (instr >> 9) & 0x7;
-
-    reg[dr] = sign_extend((instr & 0x1FF), 9);
-
-    update_flags(reg, dr);
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+   
+    reg[r0] = mem_read(reg[R_PC] + pc_offset);
+  
+    update_flags(reg, r0);
 }
 
 void op_ldi(uint16_t reg[], uint16_t instr) {
     uint16_t dr = (instr >> 9) & 0x7;
     
-    reg[dr] = reg[reg[R_PC + sign_extend(instr & 0x1FF, 9)]];
+    reg[dr] = mem_read(mem_read(reg[R_PC] + sign_extend(instr & 0x1FF, 9)));
 
     update_flags(reg, dr);
 }
 
+void op_ldr(uint16_t reg[], uint16_t instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+    uint16_t offset = sign_extend(instr & 0x3F, 6);
+    reg[r0] = mem_read(reg[r1] + offset);
+    update_flags(reg, r0);
+}
 
+void op_lea(uint16_t reg[], uint16_t instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    reg[r0] = reg[R_PC] + pc_offset;
+    update_flags(reg, r0);
+}
 
+void op_not(uint16_t reg[], uint16_t instr) {
+    uint16_t dr = (instr >> 9) & 0x7;
+    uint16_t sr = (instr >> 6) & 0x7;
 
+    reg[dr] = ~reg[sr];
 
+    update_flags(reg, dr);
+}
 
+void op_st(uint16_t reg[], uint16_t instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    mem_write(reg[R_PC] + pc_offset, reg[r0]);
+}
 
+void op_sti(uint16_t reg[], uint16_t instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    mem_write(mem_read(reg[R_PC] + pc_offset), reg[r0]);
+}
 
-
-
+void op_str(uint16_t reg[], uint16_t instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+    uint16_t offset = sign_extend(instr & 0x3F, 6);
+    mem_write(reg[r1] + offset, reg[r0]);
+}
 
 
 
