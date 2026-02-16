b2fa2ea65cda9458f0154ec4269e32303cccb2a1
Yves Kloeckner <hosta.gruen@gmail.com>
2026-02-15 23:51:12 -0700

    add typedef for u16 for clarity

--- lc3.c
+++ lc3.c
@@ -12,6 +12,8 @@
 #include <sys/select.h>
 #include <stdbool.h>
 
+typedef uint16_t u16;
+
 enum
 {
     R_R0 = 0,
@@ -69,8 +71,8 @@ enum
 #define MEMORY_MAX (1 << 16)
 
 typedef struct {
-    uint16_t mem[MEMORY_MAX];
-    uint16_t reg[R_COUNT];
+    u16 mem[MEMORY_MAX];
+    u16 reg[R_COUNT];
     int running;
 } VM;
 
@@ -97,7 +99,7 @@ void restore_input_buffering()
     tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
 }
 
-uint16_t check_key()
+u16 check_key()
 {
     fd_set readfds;
     FD_ZERO(&readfds);
@@ -113,18 +115,18 @@ void handle_interrupt(int signal)
     printf("\n");
     exit(-2);
 }
-uint16_t sign_extend(uint16_t x, int bit_count)
+u16 sign_extend(uint16_t x, int bit_count)
 {
     if ((x >> (bit_count - 1)) & 1) {
         x |= (0xFFFF << bit_count);
     }
     return x;
 }
-uint16_t swap16(uint16_t x)
+u16 swap16(uint16_t x)
 {
     return (x << 8) | (x >> 8);
 }
-void update_flags(VM* vm, uint16_t r)
+void update_flags(VM* vm, u16 r)
 {
     if (vm->reg[r] == 0)
     {
@@ -141,13 +143,13 @@ void update_flags(VM* vm, uint16_t r)
 }
 void read_image_file(VM* vm, FILE* file)
 {
-    uint16_t origin;
+    u16 origin;
     fread(&origin, sizeof(origin), 1, file);
     origin = swap16(origin);
 
-    uint16_t max_read = MEMORY_MAX - origin;
-    uint16_t* p = vm->mem + origin;
-    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+    u16 max_read = MEMORY_MAX - origin;
+    u16* p = vm->mem + origin;
+    size_t read = fread(p, sizeof(u16), max_read, file);
 
     while (read-- > 0)
     {
@@ -163,12 +165,12 @@ int read_image(VM* vm, const char* image_path)
     fclose(file);
     return 1;
 }
-void mem_write(VM* vm, uint16_t address, uint16_t val)
+void mem_write(VM* vm, u16 address, uint16_t val)
 {
     vm->mem[address] = val;
 }
 
-uint16_t mem_read(VM* vm, uint16_t address)
+u16 mem_read(VM* vm, uint16_t address)
 {
     if (address == MR_KBSR)
     {
@@ -185,47 +187,47 @@ uint16_t mem_read(VM* vm, uint16_t address)
     return vm->mem[address];
 }
 
-typedef void (*op_fn)(VM* vm, uint16_t instr);
+typedef void (*op_fn)(VM* vm, u16 instr);
 
-static void op_bad(VM* vm, uint16_t instr) {
+static void op_bad(VM* vm, u16 instr) {
     fprintf(stderr, "Illegal/Unimplemented opcode at PC=0x%04X instr=0x%04x\n",
             (unsigned) (vm->reg[R_PC] - 1), (unsigned) instr);
     vm->running = 0;
 } 
 
-static void op_add(VM* vm, uint16_t instr)
+static void op_add(VM* vm, u16 instr)
 {
-    uint16_t dr = (instr >> 9) & 0x7;
-    uint16_t sr1 = (instr >> 6) & 0x7;
-    uint16_t imm_flag = (instr >> 5) & 0x1;
+    u16 dr = (instr >> 9) & 0x7;
+    u16 sr1 = (instr >> 6) & 0x7;
+    u16 imm_flag = (instr >> 5) & 0x1;
 
     if (imm_flag) {
-        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+        u16 imm5 = sign_extend(instr & 0x1F, 5);
         vm->reg[dr] = vm->reg[sr1] + imm5;
     } else {
-        uint16_t sr2 = instr & 0x7;
+        u16 sr2 = instr & 0x7;
         vm->reg[dr] = vm->reg[sr1] + vm->reg[sr2];
     }
     update_flags(vm, dr);
 }
 
-static void op_br(VM* vm, uint16_t instr)
+static void op_br(VM* vm, u16 instr)
 {
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
-    uint16_t cond_flag = (instr >> 9) & 0x7;
+    u16 pc_offset = sign_extend(instr & 0x1FF, 9);
+    u16 cond_flag = (instr >> 9) & 0x7;
     if (cond_flag & vm->reg[R_COND]) {
         vm->reg[R_PC] += pc_offset;
     }
 }
 
-static void op_trap(VM* vm, uint16_t instr)
+static void op_trap(VM* vm, u16 instr)
 {
     vm->reg[R_R7] = vm->reg[R_PC];
 
     switch (instr & 0xFF)
     {
         case TRAP_GETC:
-            vm->reg[R_R0] = (uint16_t)getchar();
+            vm->reg[R_R0] = (u16)getchar();
             update_flags(vm, R_R0);
             break;
         case TRAP_OUT:
@@ -233,7 +235,7 @@ static void op_trap(VM* vm, uint16_t instr)
             fflush(stdout);
             break;
         case TRAP_PUTS: {
-            uint16_t* c = vm->mem + vm->reg[R_R0];
+            u16* c = vm->mem + vm->reg[R_R0];
             while (*c) { putc((char)*c, stdout); ++c; }
             fflush(stdout);
         } break;
@@ -242,11 +244,11 @@ static void op_trap(VM* vm, uint16_t instr)
             char c = getchar();
             putc(c, stdout);
             fflush(stdout);
-            vm->reg[R_R0] = (uint16_t)c;
+            vm->reg[R_R0] = (u16)c;
             update_flags(vm, R_R0);
         } break;
         case TRAP_PUTSP: {
-            uint16_t* c = vm->mem + vm->reg[R_R0];
+            u16* c = vm->mem + vm->reg[R_R0];
             while (*c) {
                 char char1 = (*c) & 0xFF;
                 putc(char1, stdout);
@@ -266,108 +268,108 @@ static void op_trap(VM* vm, uint16_t instr)
     }
 }
 
-static void op_and(VM* vm, uint16_t instr)
+static void op_and(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t r1 = (instr >> 6) & 0x7;
-    uint16_t imm_flag = (instr >> 5) & 0x1;
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 r1 = (instr >> 6) & 0x7;
+    u16 imm_flag = (instr >> 5) & 0x1;
 
     if (imm_flag)
     {
-        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+        u16 imm5 = sign_extend(instr & 0x1F, 5);
         vm->reg[r0] = vm->reg[r1] & imm5;
     }
     else
     {
-        uint16_t r2 = instr & 0x7;
+        u16 r2 = instr & 0x7;
         vm->reg[r0] = vm->reg[r1] & vm->reg[r2];
     }
     update_flags(vm, r0);
 }
 
-static void op_not(VM* vm, uint16_t instr)
+static void op_not(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t r1 = (instr >> 6) & 0x7;
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 r1 = (instr >> 6) & 0x7;
 
     vm->reg[r0] = ~vm->reg[r1];
     update_flags(vm, r0);
 }
 
-static void op_jmp(VM* vm, uint16_t instr)
+static void op_jmp(VM* vm, u16 instr)
 {
-    uint16_t r1 = (instr >> 6) & 0x7;
+    u16 r1 = (instr >> 6) & 0x7;
     vm->reg[R_PC] = vm->reg[r1];
 }
 
-static void op_jsr(VM* vm, uint16_t instr)
+static void op_jsr(VM* vm, u16 instr)
 {
-    uint16_t long_flag = (instr >> 11) & 1;
+    u16 long_flag = (instr >> 11) & 1;
     vm->reg[R_R7] = vm->reg[R_PC];
     if (long_flag)
     {
-        uint16_t long_pc_offset = sign_extend(instr & 0x7FF, 11);
+        u16 long_pc_offset = sign_extend(instr & 0x7FF, 11);
         vm->reg[R_PC] += long_pc_offset;  /* JSR */
     }
     else
     {
-        uint16_t r1 = (instr >> 6) & 0x7;
+        u16 r1 = (instr >> 6) & 0x7;
         vm->reg[R_PC] = vm->reg[r1]; /* JSRR */
     }
 }
 
-static void op_ld(VM* vm, uint16_t instr)
+static void op_ld(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 pc_offset = sign_extend(instr & 0x1FF, 9);
     vm->reg[r0] = mem_read(vm, vm->reg[R_PC] + pc_offset);
     update_flags(vm, r0);
 }
 
-static void op_ldi(VM* vm, uint16_t instr)
+static void op_ldi(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 pc_offset = sign_extend(instr & 0x1FF, 9);
     vm->reg[r0] = mem_read(vm, mem_read(vm, vm->reg[R_PC] + pc_offset));
     update_flags(vm, r0);
 }
 
-static void op_ldr(VM* vm, uint16_t instr)
+static void op_ldr(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t r1 = (instr >> 6) & 0x7;
-    uint16_t offset = sign_extend(instr & 0x3F, 6);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 r1 = (instr >> 6) & 0x7;
+    u16 offset = sign_extend(instr & 0x3F, 6);
     vm->reg[r0] = mem_read(vm, vm->reg[r1] + offset);
     update_flags(vm, r0);
 }
 
-static void op_lea(VM* vm, uint16_t instr)
+static void op_lea(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 pc_offset = sign_extend(instr & 0x1FF, 9);
     vm->reg[r0] = vm->reg[R_PC] + pc_offset;
     update_flags(vm, r0);
 }
 
-static void op_st(VM* vm, uint16_t instr)
+static void op_st(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 pc_offset = sign_extend(instr & 0x1FF, 9);
     mem_write(vm, vm->reg[R_PC] + pc_offset, vm->reg[r0]);
 }
 
-static void op_sti(VM* vm, uint16_t instr)
+static void op_sti(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 pc_offset = sign_extend(instr & 0x1FF, 9);
     mem_write(vm, mem_read(vm, vm->reg[R_PC] + pc_offset), vm->reg[r0]);
 }
 
-static void op_str(VM* vm, uint16_t instr)
+static void op_str(VM* vm, u16 instr)
 {
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t r1 = (instr >> 6) & 0x7;
-    uint16_t offset = sign_extend(instr & 0x3F, 6);
+    u16 r0 = (instr >> 9) & 0x7;
+    u16 r1 = (instr >> 6) & 0x7;
+    u16 offset = sign_extend(instr & 0x3F, 6);
     mem_write(vm, vm->reg[r1] + offset, vm->reg[r0]);
 }
 
@@ -417,9 +419,9 @@ int main(int argc, const char* argv[])
 
     while (vm.running)
     {
-        uint16_t pc_before = vm.reg[R_PC];
-        uint16_t instr = mem_read(&vm, vm.reg[R_PC]++);
-        uint16_t op = instr >> 12;
+        u16 pc_before = vm.reg[R_PC];
+        u16 instr = mem_read(&vm, vm.reg[R_PC]++);
+        u16 op = instr >> 12;
 
         if (trace)
             printf("PC: 0x%04X Instr: 0x%04X Op: 0x%X\n", 
